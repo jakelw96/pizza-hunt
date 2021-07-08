2c8ad90cc015d6ee04a5ad79cab6cb309732713f
jakelw96 <jake.lw96@gmail.com>
2021-07-07 22:10:07 -0600

    added functionality to reply to comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply
 } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaid>
@@ -13,6 +15,11 @@ router
 // /api/comments/<pizzaid>/<commentid>
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment)
 
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
+    
 module.exports = router;
